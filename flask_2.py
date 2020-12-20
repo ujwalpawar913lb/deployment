eef2affb18ab4e6a7bff3ec553c7fdf3c3c32265
ujwalpawar913 <ujwal.pawar15@vit.edu>
2020-12-20 18:00:14 +0530

    Starting commit

--- flask_2.py
+++ flask_2.py
@@ -1,9 +1,20 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app=Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return render_template('form.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    exp = request.form.get('exp')
+    score = request.form.get('score')
+    points = request.form.get('points')
+
+    print(exp , score , points)
+
+    return 'form is submitted'
+
 
 app.run(debug=True)
